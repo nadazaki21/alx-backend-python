5e57551994d99b42ec3c757f58b81cd6a7497f6f
nadazkai21 <nadazaki019@gmail.com>
2024-08-05 23:05:05 +0300

    1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+""" Task 1 """
+import asyncio
+import random
+
+wait_random = __import__("0-basic_async_syntax").wait_random
+
+
+async def wait_n(n, max_delay):
+    """Task 1"""
+    delays_list = []
+    delay_task = asyncio.create_task(wait_random(max_delay))
+    for i in range(n):
+        delays_list.append(await delay_task)
+    return sorted(delays_list)
