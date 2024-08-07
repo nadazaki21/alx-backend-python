c4a9b3808c9bc40ddcf64b3a7951762b1cce6d4d
nadazkai21 <nadazaki019@gmail.com>
2024-08-08 00:05:14 +0300

    0

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Task 0 - Async Generator """
+import asyncio
+import random
+
+
+async def async_generator():
+    """Async Generator"""
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
