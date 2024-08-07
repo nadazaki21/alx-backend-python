102592338e50f2af6ece220a14c2c3d1cec696af
nadazkai21 <nadazaki019@gmail.com>
2024-08-08 00:12:17 +0300

    1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+""" Task 1 - Async Comprehension """
+import asyncio
+
+async_generator = __import__("0-async_generator").async_generator
+
+
+async def async_comprehension():
+    """Async Comprehension"""
+    answer = [i async for i in async_generator()]
+    return answer
