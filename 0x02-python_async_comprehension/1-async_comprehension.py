2b09cae8b2b6b7a3bacecdc04ab86e3e0f62ea3e
nadazkai21 <nadazaki019@gmail.com>
2024-08-08 00:34:26 +0300

    2

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """ Task 1 - Async Comprehension """
 import asyncio
+from typing import List
 
 async_generator = __import__("0-async_generator").async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """Async Comprehension"""
     answer = [i async for i in async_generator()]
     return answer
