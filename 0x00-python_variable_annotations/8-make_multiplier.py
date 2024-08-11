724258000234fd135b898c012a9b59279e5da56f
nadazkai21 <nadazaki019@gmail.com>
2024-08-11 15:07:35 +0300

    8

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """ Task 8 """
 
+from typing import Callable
 
-def make_multiplier(multiplier: float) -> float:
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """returns a function that multiplies a float by multiplier"""
 
     def inner_fun(x: float) -> float:
