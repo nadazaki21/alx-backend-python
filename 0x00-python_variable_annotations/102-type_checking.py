8db0fcd36a9d48bff170090d584fb0695575d705
nadazkai21 <nadazaki019@gmail.com>
2024-08-12 01:28:45 +0300

    102

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -5,6 +5,7 @@ from typing import Tuple, List
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """fixeing annoations"""
     zoomed_in: List = [item for item in lst for i in range(factor)]
     return zoomed_in
     # for in in range(factor):
