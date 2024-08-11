a0f17e402b7f451f22227ff5ef15e999b0a6cbd1
nadazkai21 <nadazaki019@gmail.com>
2024-08-11 14:56:51 +0300

    all

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -3,6 +3,8 @@
 
 
 def make_multiplier(multiplier: float) -> float:
+    """returns a function that multiplies a float by multiplier"""
+
     def inner_fun(x: float) -> float:
         return x * multiplier
 
