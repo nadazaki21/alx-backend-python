b851192b42221f9973044c00034249f8a465c040
nadazkai21 <nadazaki019@gmail.com>
2024-08-11 15:00:38 +0300

    7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -3,4 +3,9 @@
 
 
 def to_kv(k: str, v: int | float) -> tuple:
+    """returns a tuple. The first element of the
+    tuple is the string k. The second element is
+    the square of the int/float v and should be
+    annotated as a float.
+    """
     return (k, float(v**2))
