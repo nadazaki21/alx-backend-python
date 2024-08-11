9612ffbbd6f54f9659fc4b06467429a5839d20a2
nadazkai21 <nadazaki019@gmail.com>
2024-08-11 15:02:26 +0300

    7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """ Task 7 """
+from typing import Tuple
 
 
-def to_kv(k: str, v: int | float) -> tuple:
+def to_kv(k: str, v: int | float) -> Tuple[str, float]:
     """returns a tuple. The first element of the
     tuple is the string k. The second element is
     the square of the int/float v and should be
