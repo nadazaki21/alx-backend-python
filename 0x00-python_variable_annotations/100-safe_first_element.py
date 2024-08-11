5ecf0b97f6d1216fc19ff86ce4df73148163e374
nadazkai21 <nadazaki019@gmail.com>
2024-08-11 15:23:16 +0300

    100

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+""" Task 1 Adavnced (100)"""
+from typing import Sequence, Union, Any
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    if lst:
+        return lst[0]
+    else:
+        return None
