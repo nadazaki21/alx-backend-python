8e4a556f927424ad193f8f55e1eb8499c2df07d6
nadazkai21 <nadazaki019@gmail.com>
2024-08-11 15:17:33 +0300

    8

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Task 9 """
-from typing import List, Tuple, Union
+from typing import List, Tuple, Iterable, Sequence
 
 
-def element_length(lst: str) -> List[Tuple[Union[str, int]]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Iterable[Sequence], int]]:
     return [(i, len(i)) for i in lst]
