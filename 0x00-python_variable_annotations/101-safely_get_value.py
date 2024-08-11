d965101960fad621777e3b00ce2dba000bdac1a0
nadazkai21 <nadazaki019@gmail.com>
2024-08-12 00:53:15 +0300

    101

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+""" Task 2 advanced (101)"""
+
+from typing import Mapping, Any, Union, TypeVar
+
+T = TypeVar("T", contravariant=True)
+
+
+def safely_get_value(
+    dct: Mapping, key: Any, default: Union[T, None] = None
+) -> Union[Any, T]:
+    if key in dct:
+        return dct[key]
+    else:
+        return default
--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+""" Task 3 advaced (102)"""
+
+from typing import Tuple, List
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    zoomed_in: Tuple[list] = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3.0)
